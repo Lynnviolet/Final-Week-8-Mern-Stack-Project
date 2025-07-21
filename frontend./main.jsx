cb327052128b77c5de96edecc3a46de99c6d6428
LYNN VIOLET WANJIRU <shikkslynn@gmail.com>
2025-07-21 16:09:51 +0300

    Create main.jsx

---
+++ frontend./main.jsx
@@ -0,0 +1,8 @@
+// src/main.jsx
+import './sentry'; // Must come before any other app logic
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
