70d983bf9b7efff2b7710144bce607b522a1a5c6
LYNN VIOLET WANJIRU <shikkslynn@gmail.com>
2025-07-24 22:40:38 +0300

    Update index.js

--- backemd/index.js
+++ backemd/index.js
@@ -1,13 +1,25 @@
-// server.js
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-const useSentry = require('./sentry');
-useSentry(app);
+app.use(cors());
+app.use(express.json());
 
-// Your routes here...
+// Sample route
 app.get('/', (req, res) => {
-  res.send('Hello from backend');
+  res.send('FitTrack API is running');
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
+
