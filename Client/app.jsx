973275ea526b7f8da380c4311297ae5ecebb5186
LYNN VIOLET WANJIRU <shikkslynn@gmail.com>
2025-07-24 22:02:33 +0300

    Create app.jsx

---
+++ Client/app.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-800">Welcome to FitTrack 🏋️‍♀️</h1>
+    </div>
+  );
+}
+
+export default App;
